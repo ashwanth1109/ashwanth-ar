baaf6e1a9f58011df8e8effdf99cdd309db5e9a1
ashwanth1109 <ashwanth1109@gmail.com>
2019-05-26 23:59:57 +0530

    Using contact details on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,52 @@
 // @flow
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "redux-first-router-link";
 import { connect } from "react-redux";
 
-import { Header as StyledHeader, Logo, Menu, MenuOption } from "./styles";
+import {
+  Header as StyledHeader,
+  VisibleHeader,
+  Logo,
+  Menu,
+  MenuOption
+} from "./styles";
 
 import logo from "assets/logo.svg";
 import { headerOptions } from "data";
 import { string } from "utils";
+import ContactDetails from "./ContactDetails";
 
 type Props = {
   currentPage: string
 };
 
 const Header = ({ currentPage }: Props) => {
+  const [contactMenu, setContactMenu] = useState(false);
   return (
-    <StyledHeader>
-      <Logo src={logo} />
-      <Menu>
-        {headerOptions.map((option, id) => (
-          <Link
-            key={id}
-            to={{
-              type: option.id,
-              payload: {}
-            }}
-          >
-            <MenuOption selected={option.id === currentPage}>
-              {string.capitalizeString(option.id)}
-            </MenuOption>
-          </Link>
-        ))}
-      </Menu>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <ContactDetails show={contactMenu} />
+        <Logo src={logo} onClick={() => setContactMenu(prev => !prev)} />
+        <VisibleHeader>
+          <Menu>
+            {headerOptions.map((option, id) => (
+              <Link
+                key={id}
+                to={{
+                  type: option.id,
+                  payload: {}
+                }}
+              >
+                <MenuOption selected={option.id === currentPage}>
+                  {string.capitalizeString(option.id)}
+                </MenuOption>
+              </Link>
+            ))}
+          </Menu>
+        </VisibleHeader>
+      </StyledHeader>
+    </>
   );
 };
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,17 +8,25 @@ import { colors, fonts } from "styles/constants";
 
 export const Header = styled.div`
   width: 100%;
-  height: 60px;
+  position: fixed;
+  background: ${colors.darkNight};
+`;
+
+export const VisibleHeader = styled.div`
+  height: 76px;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: flex-end;
-  padding: 0 24px;
-  margin-top: 16px;
+  padding: 16px 24px;
 `;
 
 export const Logo = styled(StyledLogo)`
   width: 60px;
   height: 60px;
+  margin-top: 8px;
+  position: absolute;
+  left: 16px;
+  cursor: pointer;
 `;
 
 export const Menu = styled.div`
@@ -30,7 +38,8 @@ export const MenuOption = styled.div`
   padding: 0 3vw;
   display: flex;
   color: ${colors.pureWhite};
-  font-family: ${fonts.secondary};
+  font-family: ${fonts.primary};
+  font-size: 20px;
   border-bottom: 5px solid
     ${ifProp("selected", colors.goldenYellow, colors.slabGray)};
   align-items: center;
@@ -49,4 +58,39 @@ export const MenuOption = styled.div`
       `
     )}
   }
+`;
+
+export const Contact = styled.div`
+  height: ${ifProp("show", "60px", "0px")};
+  width: 100vw;
+  transition: 0.5s ease-in-out height;
+  position: relative;
+`;
+
+export const Tray = styled.div`
+  background-color: ${colors.slabGray};
+  position: absolute;
+  width: 100vw;
+  height: 60px;
+  top: ${ifProp("show", "0px", "-60px")};
+  transition: 0.5s ease-in-out top;
+  font-size: 32px;
+  font-family: ${fonts.primary};
+  display: flex;
+  align-items: center;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const Icon = styled.img`
+  height: 40px;
+  margin-right: 16px;
+`;
+
+export const IconTitle = styled.div`
+  text-decoration: underline;
 `;
