e100d7703da3a1ce67c8951abcc43cd791bcb750
ashwanth1109 <ashwanth1109@gmail.com>
2019-05-27 00:02:46 +0530

    Added icon pack assets for skills

---
+++ src/assets/icons/index.js
@@ -0,0 +1,48 @@
+// @flow
+
+// ICON PACK 0
+import github from "./github.svg";
+import linkedin from "./linkedin.svg";
+
+// ICON PACK 1
+import html from "./html.svg";
+import css from "./css.svg";
+import js from "./js.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import firebase from "./firebase.svg";
+import saga from "./saga.svg";
+
+// ICON PACK 2
+import git from "./git.svg";
+import node from "./node.svg";
+import express from "./express.svg";
+import mongo from "./mongo.svg";
+import next from "./next.svg";
+import jquery from "./jquery.svg";
+import sketch from "./sketch.svg";
+
+export const iconPack0 = [
+  { name: "github", src: github },
+  { name: "linkedin", src: linkedin }
+];
+
+export const iconPack1 = [
+  { name: "html5", src: html },
+  { name: "css3", src: css },
+  { name: "javascript", src: js },
+  { name: "react", src: react },
+  { name: "redux", src: redux },
+  { name: "firebase", src: firebase },
+  { name: "saga", src: saga }
+];
+
+export const iconPack2 = [
+  { name: "git", src: git },
+  { name: "node", src: node },
+  { name: "express", src: express },
+  { name: "mongo", src: mongo },
+  { name: "next", src: next },
+  { name: "jquery", src: jquery },
+  { name: "sketch", src: sketch }
+];
