4790867390533d57b350c7cda6c3eb7d8d92069b
ashwanth1109 <ashwanth1109@gmail.com>
2019-06-01 02:51:48 +0530

    Made about page responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,41 @@
 // @flow
 
 import React from "react";
+import { connect } from "react-redux";
 
 import { AboutPage } from "./styles";
 import Info from "components/Info";
 import Skills from "components/Skills";
 import { Container } from "styles";
 
-// type Props = {
-//   //
-// };
+type Props = {
+  width: number
+};
 
-const About = () => {
+const About = ({ width }: Props) => {
   return (
     <AboutPage>
-      <Container flexValue={1}>
-        <Info />
-      </Container>
-      <Container flexValue={2}>
-        <Skills />
-      </Container>
+      {width > 1000 ? (
+        <>
+          <Container flexValue={1}>
+            <Info />
+          </Container>
+          <Container flexValue={2}>
+            <Skills />
+          </Container>
+        </>
+      ) : (
+        <Container flex="column" margin="0 16px">
+          <Info />
+          <Skills />
+        </Container>
+      )}
     </AboutPage>
   );
 };
 
-export default About;
+const mapStateToProps = ({ app }) => ({
+  width: app.viewport.w
+});
+
+export default connect(mapStateToProps)(About);
