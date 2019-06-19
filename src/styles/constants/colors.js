5ea4ad00ae7e614486035e81ef913ab890e8ec93
ashwanth1109 <ashwanth1109@gmail.com>
2019-06-20 01:14:57 +0530

    Added charcoal color

--- src/styles/constants/colors.js
+++ src/styles/constants/colors.js
@@ -2,5 +2,7 @@
 
 export const darkNight = "#141414";
 export const slabGray = "#292929";
+export const charcoal = "#444444";
+
 export const pureWhite = "#fff";
 export const goldenYellow = "#f5ba18";
