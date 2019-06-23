76dc4fe907faa35d8686b9543e752ebef587f26e
ashwanth1109 <ashwanth1109@gmail.com>
2019-06-23 22:29:15 +0530

    Added hello world python

---
+++ src/articles/Lectures/IntroToCS/hello.py
@@ -0,0 +1,26 @@
+# HELLO PYTHON
+
+# HELLO WORLD
+msg = "Hello world"
+print(msg)
+
+# STRING CONCATENATION
+hi = "hello there"
+name = "bardock"
+greeting = hi + " " + name
+print(greeting)
+
+# REPEAT STRING N TIMES
+print((greeting + " ") * 3)
+
+# PRINT - COMMAS AND PLUSES
+print("hello", "world")  # O/P => hello world
+print("hello" + "world")  # O/P => helloworld
+
+# GETTING INPUT FROM USER
+text = input("Enter something . . .")
+print(text)
+
+# TYPE CASTING
+num = int(input('Enter number: '))
+print(num*5)
