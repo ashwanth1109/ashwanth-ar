d23ed88d89a3fc7f93a33eab36a7d60cc4875c34
ashwanth1109 <ashwanth1109@gmail.com>
2019-06-23 22:30:19 +0530

    Added program for functions

---
+++ src/articles/Lectures/IntroToCS/functions.py
@@ -0,0 +1,30 @@
+# FUNCTIONS
+
+# # VARIABLE SCOPE
+
+
+# def f(x):
+#     x = x+1
+#     print('inside function, x is', x)  # 4
+#     return x
+
+
+# x = 3
+# z = f(x)
+# print('outside function, x is', x)  # 3
+# print('z is', z)  # 4
+
+
+# # NON FUNCTIONAL PROGRAMMING - UnboundLocalError
+# a = 5
+
+
+# def fun2():
+#     print('value of a before increment', a)
+#     a += 1
+#     print('value of a after increment', a)
+
+
+# print('value of a in global before function call', a)
+# fun2()
+# print('value of a in global after function call', a)
